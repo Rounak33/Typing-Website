d081db19ff59e63297548511444898a4cc2bb763
Rounak33 <rounak.rai33@gmail.com>
2022-12-18 13:00:43 +0530

    first commit

---
+++ src/Utils/theme.js
@@ -0,0 +1,44 @@
+const nebulaTheme = {
+    label: 'Nebula',
+    background: '#212135',
+    title: '#BE3C88',
+    typeBoxText: '#19B3B8'
+
+}
+
+const horizonTheme = {
+    label: 'Horizon',
+    background: '#1C1E26',
+    title: '#E4A88A',
+    typeBoxText: '#BBBBBB'
+}
+
+const superUserTheme = {
+    label: 'Super User',
+    background: '#262A33',
+    title: '#43FFAF',
+    typeBoxText: '#526777'
+}
+
+
+const aetherTheme = {
+    label: 'Aether',
+    background: '#101820',
+    title: '#EEDAEA',
+    typeBoxText: '#C4436F'
+}
+
+const miamiNightsTheme = {
+    label: 'Miami-Nights Theme',
+    background: '#18181A',
+    title: '#FFD3A1',
+    typeBoxText: '#3ABAC0'
+}
+
+export const themeOptions = [
+    {value: nebulaTheme, label: 'Nebula Theme'},
+    {value: horizonTheme, label: 'Horizon Theme'},
+    {value: superUserTheme, label: 'Super User'},
+    {value: aetherTheme, label: 'Aether Theme'},
+    {value: miamiNightsTheme, label: 'Miami-Nights Theme'}
+];
